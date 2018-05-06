4d6c11468d699c6ca302de535225c0c9cd90fcc1
steveyan <tinggengyan@gmail.com>
2018-05-06 21:46:13 +0800

    开篇

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ My learning process  of Cpp
 
 ----
 
+* netease 
+(网易云课堂)[http://study.163.com/course/courseMain.htm?courseId=271005]
+
---
+++ netease/1_1hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "!Hello world!" << "i am 18" << "today" << endl;
+    int age;
+    cin >> age;
+    cout << "you are " << age << "years old!";
+    return 0;
+}
