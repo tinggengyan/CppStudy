70ccb9820e7f7af30808131c48b5275b184b9443
yantinggeng <tinggeng.ytg@alibaba-inc.com>
2019-08-25 23:18:43 +0800

    clean

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # CppStudy
-My learning process  of Cpp 
 
-----
-
-* netease 
-(网易云课堂)[http://study.163.com/course/courseMain.htm?courseId=271005]
 
--- netease/1_1hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "!Hello world!" << "i am 18" << "today" << endl;
-    int age;
-    cin >> age;
-    cout << "you are " << age << "years old!";
-    return 0;
-}
