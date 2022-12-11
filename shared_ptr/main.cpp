44d6899d6c728205e456877b740374599cb4a6a3
yantinggeng <yantinggeng@bytedance.com>
2022-12-11 20:46:36 +0800

    [shared_ptr]:update shared_ptr and make_shared

--- shared_ptr/main.cpp
+++ shared_ptr/main.cpp
@@ -24,6 +24,45 @@ std::shared_ptr<Request> methodA() {
     return ptr;
 }
 
+void diff_between_constructor_make(){
+    Request *feo = new Request();
+    // 参数是 泛型的 指针
+    std::shared_ptr<Request> sharedPtrA(feo);
+    std::shared_ptr<Request> sharedPtrA_1(feo);
+    // 参数是 泛型的 实例
+    std::shared_ptr<Request> sharedPtrB = std::make_shared<Request>((*feo));
+    sharedPtrB->name = "customName";
+
+    std::cout << std::endl;
+    std::cout << "--- 原始对象值 --- " << std::endl;
+    std::cout << "raw address: " << feo << std::endl;
+
+    // 两个引用自身的值比较
+    std::cout << std::endl;
+    std::cout << "--- 两个引用自身的值比较 --- " << std::endl;
+    std::cout << "sharedPtrA address: " << sharedPtrA << std::endl;
+    std::cout << "sharedPtrB address: " << sharedPtrB << std::endl;
+
+    // 两个引用返回值比较
+    std::cout << std::endl;
+    std::cout << "--- 两个引用返回值比较 --- " << std::endl;
+    std::cout << "sharedPtrA get return address: " << sharedPtrA.get() << std::endl;
+    std::cout << "sharedPtrB get return address: " << sharedPtrB.get() << std::endl;
+
+    // 两个两个引用是否指向同一个对象？答案，不是
+    std::cout << std::endl;
+    std::cout << "--- 两个两个引用是否指向同一个对象？ --- " << std::endl;
+    std::cout << "sharedPtrA name " << sharedPtrA->name << std::endl;
+    std::cout << "sharedPtrB name " << sharedPtrB->name << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "--- 两个引用的计数 --- " << std::endl;
+    std::cout << "sharedPtrA use count: " << sharedPtrA.use_count() << std::endl;
+    std::cout << "sharedPtrA_1 use count: " << sharedPtrA_1.use_count() << std::endl;
+    std::cout << "sharedPtrB use count: " << sharedPtrB.use_count() << std::endl;
+}
+
+
 int main() {
 
     const std::shared_ptr<Request> &ptr = methodA();
