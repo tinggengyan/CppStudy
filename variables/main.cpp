1a27e5a714592b93afc3349665a04e51ce85d6b8
Tinggeng Yan <tinggengyan@gmail.com>
2023-03-08 14:05:46 +0800

    feature(variable): update variable

--- variables/main.cpp
+++ variables/main.cpp
@@ -3,7 +3,7 @@
 //
 
 #include <iostream>
-
+#include <climits>
 
 //枚举类型
 enum Color {
@@ -13,29 +13,48 @@ enum Color {
 
 typedef int AGE;
 
+// 声明
 extern int customValue;
 
+// 定义
+int customValue = 9;
 
 void init() {
-
     customValue = 10;
 }
+
+void types() {
+
+    int16_t int16t = 1;
+    uint16_t uint16t = 0;
+    long long l2 = 0;
+    long l1 = 0;
+
+
+    std::cout << "int16t:" << int16t << std::endl;
+    std::cout << "uint16t:" << uint16t << std::endl;
+
+    std::cout << "USHRT_MAX:" << USHRT_MAX << std::endl;
+    std::cout << "SHRT_MAX:" << SHRT_MAX << std::endl;
+
+};
 
 
 int main() {
 
     init();
-
-    Color red = RED;
-
-    std::cout << red << std::endl;
-
-    AGE myAge = 18;
-    std::cout << myAge << std::endl;
+//
+//    Color red = RED;
+//
+//    std::cout << red << std::endl;
+//
+//    AGE myAge = 18;
+//    std::cout << myAge << std::endl;
 
 
 //    std::cout << customValue << std::endl;
 
+    types();
 
     return 1;
 }
